5a2feee9de6ed7180d2b17b2ad69b7782f1b0663
GauravSingh07 <45252213+GauravSingh07@users.noreply.github.com>
2018-11-28 12:00:52 +0530

    Checking-in code for Sample Application

---
+++ OIDCSampleApplication/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using OIDCAppSSO.Models;
+
+namespace OIDCAppSSO.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+             LogTokenInformation();
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task LogTokenInformation()
+        {
+            // get the saved identity token
+            var identityToken = await HttpContext
+                .GetTokenAsync(OpenIdConnectParameterNames.IdToken);
+            var accessToken = await HttpContext
+               .GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+
+            // write it out
+            Debug.WriteLine($"Identity token: {identityToken}");
+
+            Debug.WriteLine($"Access token: {accessToken}");
+
+            // write out the user claims
+            foreach (var claim in User.Claims)
+            {
+                Debug.WriteLine($"Claim type: {claim.Type} - Claim value: {claim.Value}");
+            }
+
+        }
+
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("oidc");
+        }
+    }
+}
---
+++ OIDCSampleApplication/Startup.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OIDCAppSSO
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            string clientID = Configuration.GetSection("OIDCConfig").GetSection("ClientId").Value;
+            string clientSecret = Configuration.GetSection("OIDCConfig").GetSection("ClientSecret").Value;
+            string authority = Configuration.GetSection("OIDCConfig").GetSection("Authority").Value;
+
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = "Cookies";
+                    options.DefaultChallengeScheme = "oidc";
+                }).AddCookie("Cookies")
+
+
+                      .AddOpenIdConnect("oidc", options =>
+                       {
+                           options.SignInScheme = "Cookies";
+                           options.Authority = Configuration.GetSection("OIDCConfig").GetSection("Authority").Value;
+                           options.ClientId = Configuration.GetSection("OIDCConfig").GetSection("ClientId").Value; ;
+                           options.ResponseType = "code id_token";
+                           options.Scope.Add("openid");
+                           options.Scope.Add("profile");
+                           options.SaveTokens = true;
+                           options.RequireHttpsMetadata = false;
+                           options.ClientSecret = Configuration.GetSection("OIDCConfig").GetSection("ClientSecret").Value;
+                           options.GetClaimsFromUserInfoEndpoint = true;
+                       });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder();
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseAuthentication();
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
