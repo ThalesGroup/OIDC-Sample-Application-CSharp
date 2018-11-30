15c039a916376a7203138afda816f69ff014c28f
gsingh <gaurav.singh@gemalto.com>
2018-11-30 11:44:36 +0530

    Update Constants.cs

--- OIDCSampleApplication/Utilities/Constants.cs
+++ OIDCSampleApplication/Utilities/Constants.cs
@@ -8,6 +8,8 @@
         public const string Authority = "Authority";
         public const string Cookies = "Cookies";
         public const string Oidc = "oidc";
+        //For Only Authorization Flow,  it should be public const string ResponseType = "code";
+        //For Only Implicit Flow, it should be public const string ResponseType = "id_token";
         public const string ResponseType = "code id_token";
         public const string Openid = "openid";
         public const string Profile = "profile";
