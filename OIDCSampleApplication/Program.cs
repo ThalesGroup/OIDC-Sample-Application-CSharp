484c09e5463a57127a813f1736cd7406a7ad59f8
amagrawa <amit.agrawal@gemalto.com>
2018-12-21 11:14:31 +0530

    Add config file so that we can mount it while creating container

--- OIDCSampleApplication/Program.cs
+++ OIDCSampleApplication/Program.cs
@@ -1,19 +1,49 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
 
 namespace OIDCAppSSO
 {
     public class Program
     {
+        public static IConfiguration Configuration { get; set; }
+
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, configurationBuilder) =>
+                {
+                    Configuration =
+                        BuildApplicationConfiguration(hostingContext.HostingEnvironment, configurationBuilder);
+                })
+                .UseStartup<Startup>()
+                .Build();
 
-           WebHost.CreateDefaultBuilder(args)
-               .UseStartup<Startup>()
-               .Build();
+        public static IConfiguration BuildApplicationConfiguration(IHostingEnvironment hostingEnvironment,
+            IConfigurationBuilder configurationBuilder)
+        {
+            if (configurationBuilder == null)
+            {
+                configurationBuilder = new ConfigurationBuilder();
+            }
+
+            if (hostingEnvironment.IsDevelopment())
+            {
+                configurationBuilder.SetBasePath(hostingEnvironment.ContentRootPath).AddJsonFile(@"Config/config.json");
+            }
+            else
+            {
+                configurationBuilder.SetBasePath(hostingEnvironment.ContentRootPath).AddJsonFile(
+                    Environment.GetEnvironmentVariable("CONFIG_FILE_PATH"), optional: false, reloadOnChange: true);
+            }
+
+            IConfiguration configuration = configurationBuilder.Build();
+            return configuration;
+        }
     }
 }
--- OIDCSampleApplication/Startup.cs
+++ OIDCSampleApplication/Startup.cs
@@ -19,34 +19,29 @@ namespace OIDCAppSSO
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-
             services.AddAuthentication(options =>
                 {
                     options.DefaultScheme = Constants.Cookies;
                     options.DefaultChallengeScheme = Constants.Oidc;
                 }).AddCookie(Constants.Cookies)
-
-
-                      .AddOpenIdConnect(Constants.Oidc, options =>
-                       {
-                           options.SignInScheme = Constants.Cookies;
-                           options.Authority = Configuration.GetSection(Constants.OIDCConfig).GetSection(Constants.Authority).Value;
-                           options.ClientId = Configuration.GetSection(Constants.OIDCConfig).GetSection(Constants.ClientId).Value;
-                           options.ResponseType = Configuration.GetSection(Constants.OIDCConfig).GetSection(Constants.ResponseType).Value;
-                           options.Scope.Add(Constants.Openid);
-                           options.Scope.Add(Constants.Profile);
-                           options.SaveTokens = true;
-                           options.RequireHttpsMetadata = true;
-                           options.ClientSecret = Configuration.GetSection(Constants.OIDCConfig).GetSection(Constants.ClientSecret).Value;
-                           options.GetClaimsFromUserInfoEndpoint = true;
-                       });
+                  .AddOpenIdConnect(Constants.Oidc, options =>
+                  {
+                      options.SignInScheme = Constants.Cookies;
+                      options.Authority = Configuration.GetSection(Constants.OIDCConfig).GetSection(Constants.Authority).Value;
+                      options.ClientId = Configuration.GetSection(Constants.OIDCConfig).GetSection(Constants.ClientId).Value;
+                      options.ResponseType = Configuration.GetSection(Constants.OIDCConfig).GetSection(Constants.ResponseType).Value;
+                      options.Scope.Add(Constants.Openid);
+                      options.Scope.Add(Constants.Profile);
+                      options.SaveTokens = true;
+                      options.RequireHttpsMetadata = true;
+                      options.ClientSecret = Configuration.GetSection(Constants.OIDCConfig).GetSection(Constants.ClientSecret).Value;
+                      options.GetClaimsFromUserInfoEndpoint = true;
+                  });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            var builder = new ConfigurationBuilder();
-
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
@@ -58,9 +53,7 @@ namespace OIDCAppSSO
             }
 
             app.UseAuthentication();
-
             app.UseStaticFiles();
-
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
